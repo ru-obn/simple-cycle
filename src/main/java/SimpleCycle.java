fe051cd65871cf7ac36eeaad04352e2968473374
ru-obn <rufina86@bk.ru>
2020-03-22 19:50:11 +0300

    New task simple cycle

---
+++ src/main/java/SimpleCycle.java
@@ -0,0 +1,7 @@
+public class SimpleCycle {
+    public static void main(String[] args) {
+        for (int number = 0; number < 101; number = number + 1) {
+            System.out.println(number);
+        }
+    }
+}
