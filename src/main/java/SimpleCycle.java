5e9ccf3fa5ff9fc130cccb1d1b79a92396d8cf97
ru-obn <rufina86@bk.ru>
2020-03-22 23:32:42 +0300

    modification of terms and conditions

--- src/main/java/SimpleCycle.java
+++ src/main/java/SimpleCycle.java
@@ -1,6 +1,6 @@
 public class SimpleCycle {
     public static void main(String[] args) {
-        for (int number = 0; number < 101; number = number + 1) {
+        for (int number = 0; number <= 100; number++) {
             System.out.println(number);
         }
     }
